67a206a0942d4a64daec674f6725c2565dcde1c3
Ujjwal Silwal <ujjwalsilwal321@gmail.com>
2026-06-11 21:21:51 +0545

    saadasd

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
